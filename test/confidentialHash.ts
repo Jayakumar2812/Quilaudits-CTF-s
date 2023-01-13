4596fd09bd72fcead5f64c5672fc2cfbcb69e600
Jayakumar2812 <jayakumargowtham2812@gmail.com>
2023-01-13 08:20:58 +0530

    INIT Commit added ConfidentialHash

---
+++ test/confidentialHash.ts
@@ -0,0 +1,34 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Lock", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deploymentState() {
+
+    // Contracts are deployed using the first signer/account by default
+    const [owner, attacker] = await ethers.getSigners();
+
+    const Confidential = await ethers.getContractFactory("Confidential");
+    const confidential = await Confidential.deploy();
+
+    return { confidential,owner, attacker };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the right unlockTime", async function () {
+      const {confidential} = await loadFixture(deploymentState);
+      
+      const key1 = await ethers.provider.getStorageAt(confidential.address,4);
+      const key2 = await ethers.provider.getStorageAt(confidential.address,9);
+
+      const hash = await confidential.hash(key1,key2);
+
+      const checkthehash_tx = await confidential.checkthehash(hash);
+      await expect(checkthehash_tx).to.equal(true)
+    });
+
+  })
+})
